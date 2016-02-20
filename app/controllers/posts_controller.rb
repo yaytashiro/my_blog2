cfb779dff93738da76c5800ed38496f47aa9a772
田代やよい <o.841.312@gmail.com>
2016-02-20 14:22:38 +0900

    edit strongパラメーター　etc....

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,26 +6,39 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+	
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to @post
+	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
 	def create
-		
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to @post
 	end
 
-	def edit
-		
-	end
 
-	def update
-		
-	end
-
-	def delete
+	def destroy
 		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_url
 	end
+
+	private
+	def post_params 
+		params.require(:post).permit(
+			:title, :body, :category
+		)
+	end
+
 end
